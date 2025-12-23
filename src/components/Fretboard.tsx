db77b172d7e2b0e8e7544be2280704d27bf9fe06
dspedroborges <xpedrostewart@gmail.com>
2025-12-23 20:59:57 -0300

    feat: notes of chords and notes relation

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { COLORS, getScaleByKeyAndPattern, guitarNotes, OCTAVE_MAP, type ScaleType } from "../utils/functions";
+import { COLORS, getInterval, getScaleByKeyAndPattern, guitarNotes, OCTAVE_MAP, type ScaleType } from "../utils/functions";
 import ArmNote from "./ArmNote";
+import { CHORDS } from "../utils/data";
 
 export default function Fretboard() {
     const [scale, setScale] = useState<ScaleType[]>([]);
@@ -18,6 +19,39 @@ export default function Fretboard() {
 
         return -1;
     }
+
+    const getNoteByScalePosition = (scalePosition: number) => {
+        for (const s of scale) {
+            if (s.interval.value == scalePosition) {
+                return s;
+            }
+        }
+    }
+
+    const getChordNotes = (scalePositions: number[]) => {
+        let notes = [];
+        for (let i = 0; i < scalePositions.length; i++) {
+            const note = getNoteByScalePosition(scalePositions[i]);
+            if (!note) continue;
+            notes.push(note.note);
+        }
+
+        return notes;
+    }
+
+    const getNotesRelation = (notes: string[]) => {
+        let intervals = [];
+        for (let i = 0; i < notes.length; i++) {
+            if (!notes[i + 1]) break;
+            intervals.push(getInterval(notes[0], notes[i + 1]));
+        }
+
+        return intervals;
+    }
+
+    const chordNotes = getChordNotes(CHORDS.seventhNinths[0]);
+    const chordIntervals = getNotesRelation(chordNotes);
+    console.log(chordIntervals);
 
     const getNoteName = (armNote: { value: string, variations: string[] }) => {
         let name = "";
@@ -40,8 +74,9 @@ export default function Fretboard() {
                     <div id="legend" className="flex">
                         {
                             COLORS.map((c, i) => {
-                                return (
+                                return (    
                                     <div
+                                        key={i}
                                         className="w-full p-2 text-center text-white first:rounded-l last:rounded-r"
                                         style={{ backgroundColor: c }}
                                     >{i + 1}</div>
